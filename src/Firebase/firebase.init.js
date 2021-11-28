f70e71f2bf5de0778a5752529d2a7c75f0bea857
sohelrana22 <sohel252646@gmail.com>
2021-11-29 01:51:10 +0600

    start

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvX52KNq1RFLRdqrgBDlOW6A4nG5jFSVw",
+    authDomain: "my-portfolio-1d2ff.firebaseapp.com",
+    projectId: "my-portfolio-1d2ff",
+    storageBucket: "my-portfolio-1d2ff.appspot.com",
+    messagingSenderId: "188190522625",
+    appId: "1:188190522625:web:8200b918a755ba72552f00"
+  };
+  export default firebaseConfig; 
